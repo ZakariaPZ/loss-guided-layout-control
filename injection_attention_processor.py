efa1e7196b2affe8fc6785772f27c53632340c70
ZakariaPZ <zaaabc123@gmail.com>
2024-01-21 17:19:38 -0500

    Updated injection processor to class

--- injection_attention_processor.py
+++ injection_attention_processor.py
@@ -3,12 +3,6 @@ import torch
 from typing import Optional, List
 
 
-
-
-# class InjectionAttnProcessor(AttnProcessor):
-#     def __init__(self) -> None:
-#         super().__init__()
-    
 def get_attention_scores(
     self, query: torch.Tensor, key: torch.Tensor, attention_mask: torch.Tensor = None
 ) -> torch.Tensor:
@@ -57,70 +51,81 @@ def get_attention_scores(
 
     return attention_probs
 
-def attention_processor(
-    self,
-    attn: Attention,
-    hidden_states: torch.FloatTensor,
-    encoder_hidden_states: Optional[torch.FloatTensor] = None,
-    attention_mask: Optional[torch.FloatTensor] = None,
-    temb: Optional[torch.FloatTensor] = None,
-    scale: float = 1.0,
-) -> torch.Tensor:
-    """
-    Copied heavily from https://github.com/huggingface/diffusers/blob/ac61eefc9f2fbd4d2190d5673a4fcd77da9a93ab/src/diffusers/models/attention_processor.py. 
-    """
-    print('injection')
-    residual = hidden_states
 
-    args = (scale,)
+class InjectionAttnProcessor(AttnProcessor):
+    def __init__(self,
+                 sigma_t, 
+                 context_tensor) -> None:
+        super().__init__()
+        self.sigma_t = sigma_t
+        self.context_tensor = context_tensor
+        self.test = 'HI'
+    
+    def __call__(
+        self,
+        attn: Attention,
+        hidden_states: torch.FloatTensor,
+        encoder_hidden_states: Optional[torch.FloatTensor] = None,
+        attention_mask: Optional[torch.FloatTensor] = None,
+        temb: Optional[torch.FloatTensor] = None,
+        scale: float = 1.0,
+        
+    ) -> torch.Tensor:
+        """
+        Copied heavily from https://github.com/huggingface/diffusers/blob/ac61eefc9f2fbd4d2190d5673a4fcd77da9a93ab/src/diffusers/models/attention_processor.py. 
+        """
+        print(self.test)
+        residual = hidden_states
 
-    if attn.spatial_norm is not None:
-        hidden_states = attn.spatial_norm(hidden_states, temb)
+        args = (scale,)
 
-    input_ndim = hidden_states.ndim
+        if attn.spatial_norm is not None:
+            hidden_states = attn.spatial_norm(hidden_states, temb)
 
-    if input_ndim == 4:
-        batch_size, channel, height, width = hidden_states.shape
-        hidden_states = hidden_states.view(batch_size, channel, height * width).transpose(1, 2)
+        input_ndim = hidden_states.ndim
 
-    batch_size, sequence_length, _ = (
-        hidden_states.shape if encoder_hidden_states is None else encoder_hidden_states.shape
-    )
-    attention_mask = attn.prepare_attention_mask(attention_mask, sequence_length, batch_size)
+        if input_ndim == 4:
+            batch_size, channel, height, width = hidden_states.shape
+            hidden_states = hidden_states.view(batch_size, channel, height * width).transpose(1, 2)
 
-    if attn.group_norm is not None:
-        hidden_states = attn.group_norm(hidden_states.transpose(1, 2)).transpose(1, 2)
+        batch_size, sequence_length, _ = (
+            hidden_states.shape if encoder_hidden_states is None else encoder_hidden_states.shape
+        )
+        attention_mask = attn.prepare_attention_mask(attention_mask, sequence_length, batch_size)
 
-    query = attn.to_q(hidden_states, *args)
+        if attn.group_norm is not None:
+            hidden_states = attn.group_norm(hidden_states.transpose(1, 2)).transpose(1, 2)
 
-    if encoder_hidden_states is None:
-        encoder_hidden_states = hidden_states
-    elif attn.norm_cross:
-        encoder_hidden_states = attn.norm_encoder_hidden_states(encoder_hidden_states)
+        query = attn.to_q(hidden_states, *args)
 
-    key = attn.to_k(encoder_hidden_states, *args)
-    value = attn.to_v(encoder_hidden_states, *args)
+        if encoder_hidden_states is None:
+            encoder_hidden_states = hidden_states
+        elif attn.norm_cross:
+            encoder_hidden_states = attn.norm_encoder_hidden_states(encoder_hidden_states)
 
-    query = attn.head_to_batch_dim(query)
-    key = attn.head_to_batch_dim(key)
-    value = attn.head_to_batch_dim(value)
+        key = attn.to_k(encoder_hidden_states, *args)
+        value = attn.to_v(encoder_hidden_states, *args)
 
-    attention_probs = attn.get_attention_scores(query, key, attention_mask)
+        query = attn.head_to_batch_dim(query)
+        key = attn.head_to_batch_dim(key)
+        value = attn.head_to_batch_dim(value)
 
-    hidden_states = torch.bmm(attention_probs, value)
-    hidden_states = attn.batch_to_head_dim(hidden_states)
+        attention_probs = attn.get_attention_scores(query, key, attention_mask)
 
-    # linear proj
-    hidden_states = attn.to_out[0](hidden_states, *args)
-    # dropout
-    hidden_states = attn.to_out[1](hidden_states)
+        hidden_states = torch.bmm(attention_probs, value)
+        hidden_states = attn.batch_to_head_dim(hidden_states)
 
-    if input_ndim == 4:
-        hidden_states = hidden_states.transpose(-1, -2).reshape(batch_size, channel, height, width)
+        # linear proj
+        hidden_states = attn.to_out[0](hidden_states, *args)
+        # dropout
+        hidden_states = attn.to_out[1](hidden_states)
 
-    if attn.residual_connection:
-        hidden_states = hidden_states + residual
+        if input_ndim == 4:
+            hidden_states = hidden_states.transpose(-1, -2).reshape(batch_size, channel, height, width)
 
-    hidden_states = hidden_states / attn.rescale_output_factor
+        if attn.residual_connection:
+            hidden_states = hidden_states + residual
 
-    return hidden_states
+        hidden_states = hidden_states / attn.rescale_output_factor
+
+        return hidden_states
--- pipeline.py
+++ pipeline.py
@@ -1,12 +1,13 @@
 from diffusers import StableDiffusionPipeline
 from typing import Optional, List
-from injection_attention_processor import get_attention_scores, attention_processor
+from injection_attention_processor import InjectionAttnProcessor, get_attention_scores
 import torch
 import types
 
 model = StableDiffusionPipeline.from_pretrained("CompVis/stable-diffusion-v1-4").to('cuda')
 generator = torch.Generator(device="cuda").manual_seed(647)
 
+processor = InjectionAttnProcessor(0.1, 0.1)
 
 for block in model.unet.up_blocks:
     if 'CrossAttn' in block.__class__.__name__:
@@ -14,7 +15,7 @@ for block in model.unet.up_blocks:
 
             for transformer_block in module.transformer_blocks:
                 transformer_block.attn2.get_attention_scores = types.MethodType(get_attention_scores, transformer_block.attn2)
-                transformer_block.attn2.processor.__class__.__call__ = attention_processor
+                transformer_block.attn2.set_processor(InjectionAttnProcessor(0.1, 0.1))
 
 for block in model.unet.down_blocks:
     if 'CrossAttn' in block.__class__.__name__:
@@ -22,8 +23,7 @@ for block in model.unet.down_blocks:
 
             for transformer_block in module.transformer_blocks:
                 transformer_block.attn2.get_attention_scores = types.MethodType(get_attention_scores, transformer_block.attn2)
-                transformer_block.attn2.processor.__class__.__call__ = attention_processor
-
+                transformer_block.attn2.set_processor(InjectionAttnProcessor(0.1, 0.1))
 
 
 model('A colorful parrot and a red hat', generator=generator).images[0].save('astronaut_rides_horse.png')
